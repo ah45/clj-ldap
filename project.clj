ca25c0c7367c0da2f2721e2a60d86ee71ecb845c
Adam Harper <github@adam-harper.com>
2014-10-08 15:16:37 +0100

    Remove pom.xml from repository and update project.clj for deployment

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.pntblnk/clj-ldap "0.0.9"
-  :description "Clojure ldap client (development fork of alienscience's clj-ldap)."
-  :url "https://github.com/pauldorman/clj-ldap"
+(defproject org.clojars.ah45/clj-ldap "0.0.9"
+  :description "Clojure ldap client (fork of pauldorman/clj-ldap)"
+  :url "https://github.com/ah45/clj-ldap"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.unboundid/unboundid-ldapsdk "3.1.0"]]
   :profiles {:dev {:dependencies
@@ -8,6 +8,15 @@
                     [fs "1.3.3"]
                     [org.slf4j/slf4j-simple "1.5.6"]]}}
   :aot [clj-ldap.client]
+  :pom-addition [:developers
+                 [:developer
+                  [:id "pauldorman"]
+                  [:name "Paul Dorman"]
+                  [:url "https://github.com/pauldorman"]]
+                 [:developer
+                  [:id "ah45"]
+                  [:name "Adam Harper"]
+                  [:url "https://github.com/ah45"]]]
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
