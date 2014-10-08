5a1e77b4ea6f0116b9fc6eefb9bd7a1f9677048b
Adam Harper <github@adam-harper.com>
2014-10-08 15:55:37 +0100

    Ensure bind? calls against connection pools leave connections unchanged
    Without an explicit release connections from connection pools will keep
    the new binding. This can cause errors, especially when the bind fails.

--- src/clj_ldap/client.clj
+++ src/clj_ldap/client.clj
@@ -376,9 +376,14 @@ If an LDAP connection pool object is passed as the connection argument
 the bind attempt will have no side-effects, leaving the state of the
 underlying connections unchanged."
   [connection bind-dn password]
-  (try 
-    (let [bind-result (.bind connection bind-dn password)]
-      (if (= ResultCode/SUCCESS (.getResultCode bind-result)) true false))
+  (try
+    (let [cp? (instance? LDAPConnectionPool connection)
+          c (if cp? (.getConnection connection) connection)]
+      (try
+        (let [r (.bind c bind-dn password)]
+          (= ResultCode/SUCCESS  (.getResultCode r)))
+        (catch LDAPException _ false)
+        (finally (if cp? (.releaseDefunctConnection connection c)))))
     (catch Exception _ false)))
 
 (defn get
