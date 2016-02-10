1ccdd9bf053cad790f063883743bb3502efb2da6
Adam Harper <github@adam-harper.com>
2016-02-10 10:47:56 +0000

    Bump Unboundid dependency to version 3.1.0

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Clojure ldap client (development fork of alienscience's clj-ldap)."
   :url "https://github.com/pauldorman/clj-ldap"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.unboundid/unboundid-ldapsdk "2.3.0"]]
+                 [com.unboundid/unboundid-ldapsdk "3.1.0"]]
   :dev-dependencies [[jline "0.9.94"]
                      [org.apache.directory.server/apacheds-all "1.5.5"]
                      [fs "1.1.2"]
@@ -13,4 +13,3 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"})
-
